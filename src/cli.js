5ff08d3e1cf55feac25b616fdb9b45099421f509
Suares01 <hub_suares.01@hotmail.com>
2022-03-05 15:15:10 -0300

    Add new:user command

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,12 @@
-import command from "commander";
+const command = require("commander");
+const newUser = require("./commands/new:user");
 
 const cli = command
   .name("organize-cli")
   .description(
     "CLI of organize project, a tool for you to create, open and organize your projects."
   )
-  .version("0.0.1")
-  .addCommand();
+  .version("0.0.1", "-v, --version", "see the cli version")
+  .addCommand(newUser);
 
 cli.parse(process.argv);
