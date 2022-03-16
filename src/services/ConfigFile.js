5100fab6980d70c2c38e9dfd7d47448ba5ca5ced
Suares01 <hub_suares.01@hotmail.com>
2022-03-16 18:15:35 -0300

    Separete methods in classes

--- src/services/ConfigFile.js
+++ src/services/ConfigFile.js
@@ -6,9 +6,120 @@ const {
   writeAsync,
 } = require("fs-jetpack");
 
+class User {
+  constructor(filePath) {
+    this.path = filePath;
+  }
+
+  async save({ username, password, created_at }) {
+    const configFile = await readAsync(this.path);
+
+    const configObject = JSON.parse(configFile);
+
+    configObject.users.push({
+      username,
+      password,
+      created_at,
+    });
+
+    await writeAsync(this.path, configObject, { jsonIndent: 2 });
+  }
+
+  async get(username = "") {
+    const configFile = await readAsync(this.path);
+
+    const configObject = JSON.parse(configFile);
+
+    if (username) {
+      const user = configObject.users.find((usr) => usr.username === username);
+
+      if (!user) return undefined;
+
+      return user;
+    }
+
+    return configObject.users;
+  }
+
+  async setActive(username) {
+    const configFile = await readAsync(this.path);
+
+    const configObject = JSON.parse(configFile);
+
+    configObject.activeUser = username;
+
+    await writeAsync(this.path, configObject, { jsonIndent: 2 });
+  }
+
+  async getActive() {
+    const configFile = await readAsync(this.path);
+
+    const configObject = JSON.parse(configFile);
+
+    const { activeUser } = configObject;
+
+    return activeUser;
+  }
+}
+
+class IntegratedDevelopmentEnvironment {
+  constructor(filePath) {
+    this.path = filePath;
+  }
+
+  async get() {
+    const configFile = await readAsync(this.path);
+
+    const configObject = JSON.parse(configFile);
+
+    const { ide } = configObject;
+
+    return ide;
+  }
+
+  async set(ideCli) {
+    const configFile = await readAsync(this.path);
+
+    const configObject = JSON.parse(configFile);
+
+    configObject.ide = ideCli;
+
+    await writeAsync(this.path, configObject, { jsonIndent: 2 });
+  }
+}
+
+class Token {
+  constructor(filePath) {
+    this.path = filePath;
+  }
+
+  async get() {
+    const configFile = await readAsync(this.path);
+
+    const configObject = JSON.parse(configFile);
+
+    const { token } = configObject;
+
+    return token;
+  }
+
+  async save(token) {
+    const configFile = await readAsync(this.path);
+
+    const configObject = JSON.parse(configFile);
+
+    configObject.token = token;
+
+    await writeAsync(this.path, configObject, { jsonIndent: 2 });
+  }
+}
+
 module.exports = class ConfigFile {
   constructor() {
     this.path = path(__dirname, "..", "config.json");
+    this.user = new User(this.path);
+    this.ide = new IntegratedDevelopmentEnvironment(this.path);
+    this.token = new Token(this.path);
   }
 
   async generate() {
@@ -30,95 +141,5 @@ module.exports = class ConfigFile {
     if (configFile === "file") return true;
 
     return false;
-  }
-
-  async saveUser({ username, password, created_at }) {
-    const configFile = await readAsync(this.path);
-
-    const configObject = JSON.parse(configFile);
-
-    configObject.users.push({
-      username,
-      password,
-      created_at,
-    });
-
-    await writeAsync(this.path, configObject, { jsonIndent: 2 });
-  }
-
-  async getUsers(username = "") {
-    const configFile = await readAsync(this.path);
-
-    const configObject = JSON.parse(configFile);
-
-    if (username) {
-      const user = configObject.users.find((usr) => usr.username === username);
-
-      if (!user) return undefined;
-
-      return user;
-    }
-
-    return configObject.users;
-  }
-
-  async getToken() {
-    const configFile = await readAsync(this.path);
-
-    const configObject = JSON.parse(configFile);
-
-    const { token } = configObject;
-
-    return token;
-  }
-
-  async saveToken(token) {
-    const configFile = await readAsync(this.path);
-
-    const configObject = JSON.parse(configFile);
-
-    configObject.token = token;
-
-    await writeAsync(this.path, configObject, { jsonIndent: 2 });
-  }
-
-  async defineActiveUser(username) {
-    const configFile = await readAsync(this.path);
-
-    const configObject = JSON.parse(configFile);
-
-    configObject.activeUser = username;
-
-    await writeAsync(this.path, configObject, { jsonIndent: 2 });
-  }
-
-  async getActiveUser() {
-    const configFile = await readAsync(this.path);
-
-    const configObject = JSON.parse(configFile);
-
-    const { activeUser } = configObject;
-
-    return activeUser;
-  }
-
-  async getIde() {
-    const configFile = await readAsync(this.path);
-
-    const configObject = JSON.parse(configFile);
-
-    const { ide } = configObject;
-
-    return ide;
-  }
-
-  async setIde(ideCli) {
-    const configFile = await readAsync(this.path);
-
-    const configObject = JSON.parse(configFile);
-
-    configObject.ide = ideCli;
-
-    await writeAsync(this.path, configObject, { jsonIndent: 2 });
   }
 };
